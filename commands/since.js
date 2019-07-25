f4f6170da8142d35d332cda868cb6bf29e75ad6a
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-07-25 17:52:50 -0400

    git some common since

---
+++ commands/since.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const fsp = fs.promises;
+const homedir = require('os').homedir();
+const util = require('../util');
+const sh = require("run-sh");
+
+// Maximum recursion depth.
+const MAX_DEPTH = 16;
+
+module.exports = {
+    /**
+     * @param {string} tag
+     * @returns {string[]}
+     */
+    getFilesSince: async function (tag) {
+        let response = await sh(
+            "git diff --name-only " +
+            tag.replace(/[^A-Za-z0-9\\.\-_]+/, '') +
+            " HEAD"
+        );
+        return response.stdout.split("\n");
+    },
+
+    run: async function(args = []) {
+        if (args.length < 1) {
+            console.error("You must provide a version tag.");
+            process.exit(1);
+            return;
+        }
+
+        let activeDir = await fsp.realpath(process.cwd());
+        let adHash = await util.hash(activeDir);
+        let staging = await util.readJson(
+            homedir + "/.valence/" + adHash + "/staging.json"
+        );
+
+        let tag = args.shift();
+        let filesToAdd = await this.getFilesSince(tag);
+
+        for (let i = 0; i < filesToAdd.length; i++) {
+            if (staging.files.indexOf(filesToAdd[i]) < 0) {
+                staging.files.push(filesToAdd[i]);
+            }
+        }
+        await util.writeJson(
+            homedir + "/.valence/" + adHash + "/staging.json",
+            staging
+        );
+    }
+};
